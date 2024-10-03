206a9490f6e6106a8b68084c865a221867d6fa09
Dennis Frommknecht <dfrommi@users.noreply.github.com>
2024-10-03 11:42:08 +0200

    Make creation of partitions more flexible
    Allow also upfront creation of partitions

--- migrations/20240121205527_create_thing_values_tables.sql
+++ migrations/20240121205527_create_thing_values_tables.sql
@@ -20,20 +20,25 @@ CREATE INDEX idx_thing_values_tag_id_value_timestamp ON THING_VALUES (tag_id, va
 
 -- SELECT cron.schedule('weekly-partition-thing-values', '0 4 * * 3', $$ SELECT create_next_month_thing_values_partition(); $$);
 
--- Create partition for the current month
-DO $$
+CREATE OR REPLACE FUNCTION create_partition(target_year_month TEXT) RETURNS VOID AS $$
 DECLARE
     start_date TIMESTAMPTZ;
     end_date TIMESTAMPTZ;
     partition_name TEXT;
 BEGIN
-    SELECT date_trunc('MONTH', current_date) INTO start_date;
-    SELECT (date_trunc('MONTH', current_date) + INTERVAL '1 month') INTO end_date;
-    SELECT 'thing_values_' || to_char(start_date, 'YYYY_MM') INTO partition_name;
-
+    -- Parse the input string to construct the start and end dates
+    start_date := to_timestamp(target_year_month || '-01', 'YYYY-MM-DD');
+    end_date := start_date + INTERVAL '1 month';
+    
+    -- Construct the partition name based on the start date
+    partition_name := 'thing_values_' || to_char(start_date, 'YYYY_MM');
+    
+    -- Execute the SQL to create the partition and indexes
     EXECUTE 'CREATE TABLE IF NOT EXISTS ' || partition_name || ' PARTITION OF thing_values FOR VALUES FROM (''' || start_date || ''') TO (''' || end_date || ''');';
     EXECUTE 'CREATE INDEX IF NOT EXISTS idx_' || partition_name || '_timestamp ON ' || partition_name || '(timestamp);';
     EXECUTE 'CREATE INDEX IF NOT EXISTS idx_' || partition_name || '_tag_id_timestamp ON ' || partition_name || '(tag_id, timestamp DESC);';
     EXECUTE 'CREATE INDEX IF NOT EXISTS idx_' || partition_name || '_tag_id_value_timestamp ON ' || partition_name || '(tag_id, value, timestamp DESC);';
-END $$;
+END $$ LANGUAGE plpgsql;
+
+SELECT create_partition(to_char(current_date, 'YYYY-MM'));
 

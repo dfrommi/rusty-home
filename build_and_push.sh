1c827cbabcc87641df7a06e00f308ead31db9589
Dennis Frommknecht <dfrommi@users.noreply.github.com>
2025-09-11 20:38:59 +0200

    Switch to Github Container regsitry

--- build_and_push.sh
+++ build_and_push.sh
@@ -4,5 +4,5 @@ set -e
 
 cargo sqlx prepare --workspace
 
-docker buildx build --platform linux/amd64 -t home:5000/rusty-home:latest .
-docker push home:5000/rusty-home:latest
+docker buildx build --platform linux/amd64 -t ghcr.io/dfrommi/rusty-home:latest .
+docker push ghcr.io/dfrommi/rusty-home:latest

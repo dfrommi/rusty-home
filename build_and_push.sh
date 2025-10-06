8911c02acf09761434206a1e787d5388a826197e
Dennis Frommknecht <dfrommi@users.noreply.github.com>
2025-10-06 17:57:38 +0200

    Tag docker containers with git hash for rollback

--- build_and_push.sh
+++ build_and_push.sh
@@ -11,10 +11,15 @@ trap cleanup EXIT INT TERM
 
 colima start -p x86 || true
 
-docker --context colima-x86 build --platform linux/amd64 -t ghcr.io/dfrommi/rusty-home:latest $@ .
+commit_hash=$(git rev-parse --short=8 HEAD)
+
+docker --context colima-x86 build --platform linux/amd64 \
+  -t ghcr.io/dfrommi/rusty-home:latest \
+  -t ghcr.io/dfrommi/rusty-home:"${commit_hash}" \
+  "$@" .
 docker --context colima-x86 push ghcr.io/dfrommi/rusty-home:latest
+docker --context colima-x86 push ghcr.io/dfrommi/rusty-home:"${commit_hash}"
 
 ssh home -o RemoteCommand=none "cd /opt/stacks/smart-home && docker compose pull rusty-home && docker compose up -d rusty-home"
 
 docker --context home logs -f rusty-home
-

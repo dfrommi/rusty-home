eca87451ff6243f4a551756f8d602995f2761046
Dennis Frommknecht <dfrommi@users.noreply.github.com>
2025-11-03 16:17:30 +0100

    Return trace-id in API responses

--- lib/infrastructure/src/http/server.rs
+++ lib/infrastructure/src/http/server.rs
@@ -1,6 +1,12 @@
-use actix_web::*;
+use actix_web::{
+    dev::Service as _,
+    http::header::{HeaderName, HeaderValue},
+    *,
+};
 use anyhow::Context as _;
 use serde::Deserialize;
+
+use crate::TraceContext;
 
 #[derive(Debug, Deserialize, Clone)]
 pub struct HttpServerConfig {
@@ -13,8 +19,25 @@ impl HttpServerConfig {
         F: Fn() -> Vec<Scope> + Send + Clone + 'static,
     {
         let http_server = HttpServer::new(move || {
-            let mut app = App::new().wrap(tracing_actix_web::TracingLogger::default());
+            let mut app = App::new()
+                //has to be before other middlewares to capture trace id
+                .wrap_fn(|req, srv| {
+                    let fut = srv.call(req);
+                    async {
+                        let mut res = fut.await?;
 
+                        if let Some(ctx) = TraceContext::current()
+                            && let Ok(trace_id) = HeaderValue::from_str(ctx.trace_id().as_str())
+                        {
+                            res.headers_mut().insert(HeaderName::from_static("trace-id"), trace_id);
+                        }
+
+                        Ok(res)
+                    }
+                })
+                .wrap(tracing_actix_web::TracingLogger::default());
+
+            //Add all scopes (top-level resources)
             for scope in scopes() {
                 app = app.service(scope);
             }

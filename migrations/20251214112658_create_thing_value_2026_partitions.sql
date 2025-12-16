7b636c0968935129fe694d91ab429631c29b628f
Dennis Frommknecht <dfrommi@users.noreply.github.com>
2025-12-16 23:58:28 +0100

    Add migration to create partitions of 2026

---
+++ migrations/20251214112658_create_thing_value_2026_partitions.sql
@@ -0,0 +1,13 @@
+-- Create partitions for each month of 2026 for thing_value
+SELECT create_thing_value_partition('2026-01');
+SELECT create_thing_value_partition('2026-02');
+SELECT create_thing_value_partition('2026-03');
+SELECT create_thing_value_partition('2026-04');
+SELECT create_thing_value_partition('2026-05');
+SELECT create_thing_value_partition('2026-06');
+SELECT create_thing_value_partition('2026-07');
+SELECT create_thing_value_partition('2026-08');
+SELECT create_thing_value_partition('2026-09');
+SELECT create_thing_value_partition('2026-10');
+SELECT create_thing_value_partition('2026-11');
+SELECT create_thing_value_partition('2026-12');
